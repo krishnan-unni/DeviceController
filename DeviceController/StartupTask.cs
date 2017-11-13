184c56d6f3b0b982bce97cb7df66b7e954177c4f
krishnan-unni <Krishnan.Unni@outlook.com>
2017-11-13 15:30:24 -0500

    Final commit
    Completed device controller headless app for Dual Color LED

--- DeviceController/StartupTask.cs
+++ DeviceController/StartupTask.cs
@@ -1,40 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Azure.Devices.Client;
 using System.Text;
-using System.Net.Http;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
-using Microsoft.Azure.Devices.Client;
-using System.Threading.Tasks;
-
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace DeviceController
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        static string connectionString = "HostName=INetHub.azure-devices.net;DeviceId=IoTPi;SharedAccessKey=xK4k7QihqslhUNh6VEiEy41903AX0NmmMcLjPSCbZuM=";
+        static string connectionString = "<Device Primary Connection String - from IoT Hub>";
         DeviceClient client;
+        GpioController deviceController;
+        GpioPin greenPin;
+        GpioPin redPin;
+        GpioPinValue greenState;
+        GpioPinValue redState;
+
         public async void Run(IBackgroundTaskInstance taskInstance)
         {
             client = DeviceClient.CreateFromConnectionString(connectionString);
+            deviceController = GpioController.GetDefault();
+            if (deviceController != null)
+            {
+                greenPin = deviceController.OpenPin(18);
+                greenPin.SetDriveMode(GpioPinDriveMode.Output);
+                redPin = deviceController.OpenPin(27);
+                redPin.SetDriveMode(GpioPinDriveMode.Output);
+
+                greenState = GpioPinValue.High;
+                redState = GpioPinValue.Low;
+                greenPin.Write(greenState);
+                redPin.Write(redState);
+            }
+
             var deferral = taskInstance.GetDeferral();
+
+            await ExecuteCommandAsync();
 
             deferral.Complete();
         }
-
-        private async Task ExecuteCommandAsync()
+        
+        async Task ExecuteCommandAsync()
         {
             while (true)
             {
                 Message command = await client.ReceiveAsync();
-                //command.SequenceNumber
+
                 if (command != null)
                 {
-                    //process command 
+                    string cmdText = Encoding.ASCII.GetString(command.GetBytes());
+                    if (cmdText == "green")
+                    {
+                        greenPin.Write(GpioPinValue.High);
+                        redPin.Write(GpioPinValue.Low);
+                    }
+                    else if (cmdText == "red")
+                    {
+                        greenPin.Write(GpioPinValue.Low);
+                        redPin.Write(GpioPinValue.High);
+                    }
                     await client.CompleteAsync(command.LockToken);
                 }
+
             }
         }
     }
